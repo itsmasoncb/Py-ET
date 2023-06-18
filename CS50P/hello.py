6ca92b87b81d2a88e5fbef8788381f7d2f9c1e4f
Mason Brown <mason@masoncbrown.com>
2023-06-17 20:19:27 -0400

    initial learning files

---
+++ CS50P/hello.py
@@ -0,0 +1,6 @@
+# Ask user for name
+name = input("What's your name? ")
+
+# Say hello to user
+print("hello, ", end="")  # Overwrite default end='\n' for new line with nothing.
+print(name)
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Py-ET
-Linkedin Python Course Notes
+Python Course Notes (LinkedIn, edX, etc.)
