c542e295ab7d219951e3154a8950eb1f9abb641d
Mason Brown <mason@masoncbrown.com>
2023-06-19 16:49:09 -0400

    calc and hello updates

---
+++ CS50P/calc.py
@@ -0,0 +1,9 @@
+# Floating point numbers, input for both.
+x = float(input("What's x? "))
+y = float(input("What's y? "))
+
+# z for rounded default of x + y, can specify
+z = round(x + y)
+
+# f-string, print z from above and format to have a comma.
+print(f"{z:,}")
--- CS50P/hello.py
+++ CS50P/hello.py
@@ -1,6 +1,11 @@
 # Ask user for name
-name = input("What's your name? ")
+name = input("What's your name? ").strip().title()
+# Remove whitespace from str, and capitalize user's name, combined with above.
+# name = name.strip().title()
+
+# Split users name into first and last.
+first, last = name.split(" ")
 
 # Say hello to user
-print("hello, ", end="")  # Overwrite default end='\n' for new line with nothing.
-print(name)
+# print("hello, ", end="")  # Overwrite default end='\n' for new line with nothing.
+print(f"Hello, {first}")
